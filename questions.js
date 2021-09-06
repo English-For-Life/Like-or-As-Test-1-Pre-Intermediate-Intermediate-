5b8e4d01619adc1db614d753694830d3ccd78842
English-For-Life <83182532+English-For-Life@users.noreply.github.com>
2021-09-06 19:36:59 +0300

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,57 @@
+//Array of objects
+const quiz = [
+
+    {
+        q:"Do you think Ann looks ..... her mother?",
+        options:["like", "as"],
+        answer: 0
+    },
+    {
+        q:"He really gets on my nerves. I can't stand people ..... him.",
+        options:["as", "like"],
+        answer: 1
+    },
+    {
+        q:"Why didn't you do it ..... I told you to do it?",
+        options:["as", "like"],
+        answer: 0
+    },
+    {
+        q:"'Where does Bill work?' - 'He works in a bank ..... most of his friends'.",
+        options:["as", "like"],
+        answer: 1
+    },
+    {
+        q:"He never listens. Talking to him is ..... talking to a wall.",
+        options:["like", "as"],
+        answer: 0
+    },
+    {
+        q:"..... I said yesterday, I'm thinking of going to Canada.",
+        options:["Like", "As"],
+        answer: 1
+    },
+    {
+        q:"Tom's idea seemed a good one, so we did ..... he suggested.",
+        options:["as", "like"],
+        answer: 0
+    },
+    {
+        q:"It's a difficult problem. I never know what to do in situation ..... this.",
+        options:["as", "like"],
+        answer: 1
+    },
+    {
+        q:"I'll phone you tomorrow evening ..... usual, okay?",
+        options:["as", "like"],
+        answer: 0
+    },
+    {
+        q:"This coffee is terrible. It tastes ..... water.",
+        options:["as", "like"],
+        answer: 1
+    }
+
+
+
+]
